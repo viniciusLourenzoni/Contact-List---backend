d73d037f46ef7c9b39febe7e4b3ff89e4b4ea5a2
Vinicius Lourenzoni <viniciuslourenzoni0@gmail.com>
2024-05-16 22:30:42 -0300

    criado classe de produto

---
+++ src/product/product/product.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ProductResolver } from './product.resolver';
+import { ProductService } from './product.service';
+
+@Module({
+  providers: [ProductResolver, ProductService]
+})
+export class ProductModule {}
